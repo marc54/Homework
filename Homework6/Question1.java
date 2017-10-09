2d669006f6126c6c1d372d9fb55a8b431505466f
marc54 <31526703+marc54@users.noreply.github.com>
2017-10-09 16:19:20 -0400

    Add files via upload

---
+++ Homework6/Question1.java
@@ -0,0 +1,14 @@
+public class Question1{
+	public static void main(String[] args){
+			B newb = new B();
+	}
+}
+
+class A{
+	private int x =5;
+	A(){
+		System.out.println(x);
+	}
+}
+
+class B extends A{}
